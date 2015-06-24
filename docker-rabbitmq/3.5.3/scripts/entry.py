691cafa213dbaadd19c119e06f791b5b0dc171c1
Antonia Caskey <Antonia.Caskey@solnet.co.nz>
2015-06-24 13:10:17 +1200

    Removed unnecessary sections from entry.py in order to have a good template to start working with. Also, added run instructions to the Dockerfile that should be suitable for this application.

--- docker-rabbitmq/3.5.3/scripts/entry.py
+++ docker-rabbitmq/3.5.3/scripts/entry.py
@@ -32,22 +32,6 @@ def isIP(address):
       ip = False
    return ip
 
-# Define the cleanup function
-def cleanup(child):
-    # Warning: This function can be registered more than once, code defensively!
-    if child is not None: # Make sure the child actually exists
-        print "Sending SIGTERM"
-        child.terminate() # Terminate the child cleanly
-        try:
-            for line in iter(child.stdout.readline, ''): # Clear the buffer of any lines remaining
-                sys.stdout.write(line)
-        except IOError:
-            pass # No output found, resulted in IOError
-
-
-# Variables/Consts TODO: CHANGED THESE
-
-
 ########################################################################################################################
 # ARGUMENT PARSER                                                                                                      #
 # This is where you put the Argument Parser lines                                                                      #
@@ -97,204 +81,15 @@ for addr in args.member_addr:
 cluster_hosts = [args.rep_addr] + args.member_addr
 
 ########################################################################################################################
-# Initialize MySQL on first run                                                                                        #
+# Initialize RabbitMQ		                                                                                       #
 ########################################################################################################################
-# If ibdata1 does not exist under the mysql path then this is the first time the container has been run
-# so mysql_install_db needs to be run to initialize the DB using the volume mounted from a directory on the host
-# We also set up the replication DB user and the root DB user password 
-# Once run, this does not need to run again for the life of the container (assuming the DB data mount does not get blatted!)
-if not os.path.isfile(mysql_init_check_file):
-   print "No existing databases found under %s" % mysql_path
-   first_run = True
-   # Flush anything on the buffer
-   sys.stdout.flush()
-   # Reopen stdout as unbuffered. This will mean log messages will appear as soon as they become avaliable.
-   sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
-   if not os.path.isfile(mysql_default_cnf):
-      copyfile(mysql_my_cnf,mysql_default_cnf)
-   
-   print "Creating initial system tables for MySQL"
-   # Initialize system tables
-   mysql_install = Popen(['/usr/bin/mysql_install_db'], stdout = PIPE, stderr = PIPE, shell = False)
-   output, err = mysql_install.communicate() 
-   # Ignore errors for install
-
-   print output
-
-   # Start mysql
-   mysql_start = Popen(["service", "mysql", "start"], stdout = PIPE, stderr = PIPE, shell = False)
-   output, err = mysql_start.communicate()
-   if err:
-      print err
-      sys.exit(0) # This should be a return 0 to prevent the container from restarting
-
-   print output
-  
-   print "Creating replication DB user and setting password for root DB user"
-   # Open database connection
-   host = 'localhost'
-   user = 'root'
-   location = '%'
-   db = MySQLdb.connect(host,user)
-
-   # Prepare a cursor object using cursor method
-   cursor = db.cursor()
-
-   try:
-      # Set wsrep_on to OFF
-      wsrep_on_off = "SET wsrep_on=OFF"
-      results = cursor.execute(wsrep_on_off)
-      print "Setting wsrep_on to OFF returned %s" % results
-
-      # Remove user accounts with empty user names
-      remove_user_accounts = "DELETE FROM mysql.user WHERE user=''"
-      results = cursor.execute(remove_user_accounts)
-      print "Removing MySQL user accounts with empty user names returned %s" % results
-
-      # Grant privileges to replication user
-      rep_user_privs = "GRANT ALL ON *.* TO '%s'@'%s' IDENTIFIED BY '%s'" %(rep_user, location, rep_pass)
-      results = cursor.execute(rep_user_privs)
-      print "Granting privlieges to MySQL replication user %s returned %s" %(rep_user, results)
-
-      # Set root user password
-      root_user_pass = "GRANT ALL ON *.* TO '%s'@'%s' IDENTIFIED BY '%s'" %(user, location, root_pass)
-      results = cursor.execute(root_user_pass)
-      print "Setting password for MySQL %s user returned %s" %(user, results)
-
-   except MySQLdb.Error, e:
-      print e
-      sys.exit(0) # This should be a return 0 to prevent the container from restarting
-
-   # Close cursor
-   cursor.close
-   # Close connection
-   db.close
-
-   # Stop MySQL
-   mysql_stop = Popen(["service", "mysql", "stop"], stdout = PIPE, stderr = PIPE, shell = False)
-   output, err = mysql_stop.communicate()
-   if err:
-      print err
-      sys.exit(0) # This should be a return 0 to prevent the container from restarting
-
-   print output
-
 
 ########################################################################################################################
 # TEMPLATES                                                                                                            #
 # This is where you manage any templates                                                                               #
 ########################################################################################################################
-# Configuration Location goes here
-template_location = '/mysql-templates'
-
-# Create the template list
-template_list = {}
-
-# Templates go here
-### wsrep.cnf ###
-template_name = 'wsrep.cnf'
-template_dict = { 'context' : { # Subsitutions to be performed
-                                'cluster_name' : args.cluster_name,
-                                'cluster_addr' : cluster_addr,
-                                'rep_addr'     : args.rep_addr,
-                                'rep_user'     : args.rep_user,
-                                'rep_pass'     : args.rep_pass,
-                              },
-                  'path'    : '/etc/mysql/conf.d/wsrep.cnf',
-                  'user'    : 'root',
-                  'group'   : 'root',
-                  'mode'    : 0644 }
-template_list[template_name] = template_dict
-
-# Load in the files from the folder
-template_loader = FileSystemLoader(template_location)
-template_env = TemplateEnvironment(loader=template_loader,
-                                   lstrip_blocks=True,
-                                   trim_blocks=True,
-                                   keep_trailing_newline=True)
-
-# Load in expected templates
-for template_item in template_list:
-   # Attempt to load the template
-   try:
-       template_list[template_item]['template'] = template_env.get_template(template_item)
-   except TemplateNotFound as e:
-       errormsg = "The template file %s was not found in %s (returned %s)," % template_item, template_list, e
-       errormsg += " terminating..."
-       print errormsg
-       sys.exit(0) # This should be a return 0 to prevent the container from restarting
-
-   # Attempt to open the file for writing
-   try:
-       template_list[template_item]['file'] = open(template_list[template_item]['path'],'w')
-   except IOError as e:
-       errormsg = "The file %s could not be opened for writing for template" % template_list[template_item]['path']
-       errormsg += " %s (returned %s), terminating..." % template_item, e
-       print errormsg
-       sys.exit(0) # This should be a return 0 to prevent the container from restarting
-
-   # Stream
-   try:
-       template_list[template_item]['render'] = template_list[template_item]['template'].\
-                                            render(template_list[template_item]['context'])
-
-       # Submit to file
-
-       template_list[template_item]['file'].write(template_list[template_item]['render'].encode('utf8'))
-       template_list[template_item]['file'].close()
-   except:
-       e = sys.exc_info()[0]
-       print "Unrecognised exception occured, was unable to create template (returned %s), terminating..." % e
-       sys.exit(0) # This should be a return 0 to prevent the container from restarting.
-
-   # Change owner and group
-   try:
-       template_list[template_item]['uid'] = pwd.getpwnam(template_list[template_item]['user']).pw_uid
-   except KeyError as e:
-       errormsg = "The user %s does not exist for template %s" % template_list[template_item]['user'], template_item
-       errormsg += "(returned %s), terminating..." % e
-       print errormsg
-       sys.exit(0) # This should be a return 0 to prevent the container from restarting
-
-   try:
-       template_list[template_item]['gid'] = grp.getgrnam(template_list[template_item]['group']).gr_gid
-   except KeyError as e:
-       errormsg = "The group %s does not exist for template %s" % template_list[template_item]['group'], template_item
-       errormsg += "(returned %s), terminating..." % e
-       print errormsg
-       sys.exit(0) # This should be a return 0 to prevent the container from restarting
-
-   try:
-       os.chown(template_list[template_item]['path'],
-                template_list[template_item]['uid'],
-                template_list[template_item]['gid'])
-   except OSError as e:
-       errormsg = "The file %s could not be chowned for template" % template_list[template_item]['path']
-       errormsg += " %s (returned %s), terminating..." % template_item, e
-       print errormsg
-       sys.exit(0) # This should be a return 0 to prevent the container from restarting
-
-   # Change permissions
-   try:
-       os.chmod(template_list[template_item]['path'],
-                template_list[template_item]['mode'])
-   except OSError as e:
-       errormsg = "The file %s could not be chmoded for template" % template_list[template_item]['path']
-       errormsg += " %s (returned %s), terminating..." % template_item, e
-       print errormsg
-       sys.exit(0) # This should be a return 0 to prevent the container from restarting
 
 ########################################################################################################################
 # SPAWN CHILD                                                                                                          #
 ########################################################################################################################
-# Spawn the child
-child_path = ['/usr/sbin/mysqld']
-# REMOVED CONTENT
-child = Popen(child_path, stdout = PIPE, stderr = STDOUT, shell = False) 
 
-# Output any log items to Docker
-for line in iter(child.stdout.readline, ''):
-    sys.stdout.write(line)
-
-# If the process terminates, read its errorcode and return it
-sys.exit(child.returncode)
